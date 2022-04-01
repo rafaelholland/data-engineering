924f86d45debf68a81ff4bbcbf8df77bdb9eb56b
Rafael Eduardo Holland <rafa@rafa-mac.local>
2022-03-31 21:50:34 -0300

    zipped airflow folder

--- datapipeline/airflow/plugins/airflow_plugin.py
+++
@@ -1,6 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-from operators.twitter_operator import TwitterOperator
-
-class AluraAirflowPlugin(AirflowPlugin):
-    name = "alura"
-    operators = [TwitterOperator]
